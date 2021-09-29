cbaa69d0a3093fb42a6062663fa93e74649b1d23
Abdyl-Aziz <azizshihabov@mail.ru>
2021-09-29 14:23:19 +0200

    init: start project

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bulnde.js'
+  },
+
+  
+};
