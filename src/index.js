3097738a0d310bbf113b40aa3d0eeca4d3659169
Abdyl-Aziz <azizshihabov@mail.ru>
2021-09-29 14:57:49 +0200

    feat: add html babel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 console.log('Hello, World!');
+
+
+const heading = document.createElement('h1')
+heading.textContent = 'Webpack!'
+
+const root = document.querySelector('#root')
+root.append(heading)
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,42 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  CleanWebpackPlugin
+} = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
 
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    
+  ],
+
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].bulnde.js'
   },
-
-  
 };
